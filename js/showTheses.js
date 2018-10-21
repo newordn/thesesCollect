0d67801197916c6220180a0bff0d51a4ec09077f
Neword <newordn@gmail.com>
2018-10-21 18:55:50 +0200

    Update showTheses.js
    Updating the request for theseDetail and dynamically filling the template with the response

--- js/showTheses.js
+++ js/showTheses.js
@@ -1,11 +1,33 @@
-$(".detailLink").click((e)=>{
-    e.preventDefault();
-    let id = $(this).attr("href");
+
+
+
+$.each(document.getElementsByClassName("detailLink"),(i,v)=>{
+    v.addEventListener("click",()=>{
     $.ajax({
-        url: "http://192.168.43.84:8080/these/getOne/"+ id
-    })
-        .done(function (data) {
-                console.log(data);
-            
-        })
+    url: "http://localhost:8080/these/getOne/"+ v.getAttribute("href")[1]
+}).done((data)=>{
+    console.log(data);
+$("#detailDate").text(data.theseDate)
+$("#detailRegion").text(data.regions)
+$("#detailUniversity").text(data.university)
+$("#detailFaculty").text(data.faculty)
+$("#detailOption").text(data.options)
+$("#detailLevel").text(data.level)
+$("#detailSubject").text(data.subject)
+$("#detailAbstract").attr("src",data.resumes)
+$("#detailStudent").text(data.student)
+$("#detailChiefOfWork").text(data.workChief)
+$("#detailProf").text(data.profesor)
+$("#detailAssistant").text(data.assistant)
+$("#detailBibliography").html("");
+$("#detailLibrary").html("");
+data.bibliography.forEach((v)=>{
+    $("#detailBibliography").append("<li class='list-group-item'>" + v.title +"</li>");
+})
+
+data.library.forEach((v)=>{
+    $("#detailLibrary").append("<li class='list-group-item'>" + v.description +"</li>");
+})
+})
+})
 })
