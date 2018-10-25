8619ab1e5fb65f39f2a129f240ec780c00a73e0e
Neword <newordn@gmail.com>
2018-10-25 09:51:49 +0100

    Adding unchecked on users items lsit

--- js/user.js
+++ js/user.js
@@ -1,7 +1,27 @@
 $.each(document.getElementsByClassName("user-item"), (i, v) => {
     v.addEventListener("click", () => {
-        v.setAttribute("style","   background-color:#2286c3; color:white;")
-        if(v.getAttribute("value"))
-        $("#usersId").val($("#usersId").val() + v.getAttribute("value")+",");
-        console.log($("#usersId").val())
+        if (v.getAttribute("style") =="background-color:#2286c3; color:white;")
+        {
+            v.setAttribute("style", "background-color:white; color:black;")
+            let arr = $("#usersId").val().split(",")
+            console.log(arr);
+            if(v.getAttribute("value"))
+            { 
+                let index = arr.indexOf(v.getAttribute("value"));
+                
+                if(index>-1)
+                {
+                    arr.splice(index,1);
+                    console.log(arr.join(","));
+                    $("#usersId").val(arr.join(","));
+                }
+            }
+            return ;
+        }
+        else
+        { 
+            v.setAttribute("style","background-color:#2286c3; color:white;")
+            if(v.getAttribute("value"))
+            $("#usersId").val($("#usersId").val() + ","+ v.getAttribute("value"));
+        }
     })})
