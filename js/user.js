472af050a377e14ceeb110219bea255206e1e7f1
Neword <newordn@gmail.com>
2018-10-25 08:27:02 +0100

    Sending users ids these are selected for adding it into a crew with id groupeId

--- js/user.js
+++ js/user.js
@@ -1,4 +1,7 @@
 $.each(document.getElementsByClassName("user-item"), (i, v) => {
     v.addEventListener("click", () => {
         v.setAttribute("style","   background-color:#2286c3; color:white;")
+        if(v.getAttribute("value"))
+        $("#usersId").val($("#usersId").val() + v.getAttribute("value")+",");
+        console.log($("#usersId").val())
     })})
