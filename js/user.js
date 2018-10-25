9744c562a2b5620288cb5f68398ea0c0f9c781bb
Neword <newordn@gmail.com>
2018-10-25 08:16:57 +0100

    Higlighting list items on click into the list of users within users.html

---
+++ js/user.js
@@ -0,0 +1,4 @@
+$.each(document.getElementsByClassName("user-item"), (i, v) => {
+    v.addEventListener("click", () => {
+        v.setAttribute("style","   background-color:#2286c3; color:white;")
+    })})
